dc2b8613f0247ac9d397b38079e790a978605675
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-16 21:36:28 +0530

    footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div className="py-5 bg-black z-2 positon-absolute">
+            <p className="text-light">Copyright © Your Website 2024</p>
+        </div>
+    )
+}
+
+export default Footer
