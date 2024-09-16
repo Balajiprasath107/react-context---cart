66eb7015d9633cf625ff72bbbb13f00902b11683
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-16 14:34:32 +0530

    p elment changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         <div className="py-5 bg-black z-2 positon-absolute">
             <h1 className="display-3 font-weight-bold text-white ">Trends</h1>
-            <p className="h5 text-secondary">A space for Beauty and Fashion Accessories </p>
+            <p className="h5 text-secondary">Shop everything and anything</p>
         </div>
     )
 }
