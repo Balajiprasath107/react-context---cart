a0351f4e78cd23674bbb315fc472885e625eb45b
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-17 16:23:56 +0530

    add Header in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Catalog from './catalog'
+import Header from './header'
 
 
 function Home() {
     return (
         <div>
+            <Header/>
             <Catalog/>
         </div>
     )
