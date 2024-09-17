fa99305b0ac037e5f0a44225daf9edf17415559e
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-17 12:18:28 +0530

    Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Catalog from './catalog'
+
+
+function Home() {
+    return (
+        <div>
+            <Catalog/>
+        </div>
+    )
+}
+
+export default Home
