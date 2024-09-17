a8af52e59357114ce4b63cce40734a9f4d6671d3
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-17 16:23:35 +0530

    remove item and add item function create in context

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -7,9 +7,20 @@ export const CartContext = createContext(null);
 
 function Cartstate({children}){
     const [cartCount,setCartCount] = useState(0)
-    
+    const [cartContains,setCartContains] = useState([])
+    function removeitem(item){
+        let newbasket = cartContains.filter((content)=> content.id !== item.id)
+        setCartContains([...newbasket])
+        setCartCount(cartCount-1)
+    }
+    function Additem(item){
+        let newbasket = [...cartContains]
+        newbasket.push(item)
+        setCartContains([...newbasket])
+        setCartCount(cartCount+1)
+    }
     return(
-        <CartContext.Provider value={{products,cartCount,setCartCount}}>
+        <CartContext.Provider value={{products,cartCount,setCartCount,removeitem,Additem,cartContains,setCartContains}}>
             {children}
         </CartContext.Provider>
     )
