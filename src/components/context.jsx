3feffaa0659a18ab7424307dcffdde13190e1696
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-18 22:38:00 +0530

    changes and update on states in context

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -8,19 +8,23 @@ export const CartContext = createContext(null);
 function Cartstate({children}){
     const [cartCount,setCartCount] = useState(0)
     const [cartContains,setCartContains] = useState([])
+    const [TotalCartPrice,setTotalCartPrice] = useState(0)
     function removeitem(item){
         let newbasket = cartContains.filter((content)=> content.id !== item.id)
         setCartContains([...newbasket])
-        setCartCount(cartCount-1)
+        setCartCount(cartCount-item.quantity)
+        setTotalCartPrice(((TotalCartPrice*100)-((item.price*100)*item.quantity))/100)
+        item.quantity = 1
     }
     function Additem(item){
         let newbasket = [...cartContains]
         newbasket.push(item)
         setCartContains([...newbasket])
+        setTotalCartPrice(((TotalCartPrice*100)+((item.price*100)*item.quantity))/100)
         setCartCount(cartCount+1)
     }
     return(
-        <CartContext.Provider value={{products,cartCount,setCartCount,removeitem,Additem,cartContains,setCartContains}}>
+        <CartContext.Provider value={{products,cartCount,setCartCount,removeitem,Additem,cartContains,setCartContains,TotalCartPrice,setTotalCartPrice}}>
             {children}
         </CartContext.Provider>
     )
