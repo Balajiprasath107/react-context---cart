8969e6cacd608369a23c9ebe91ecfe04b817c257
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-19 15:22:57 +0530

    checkout box update

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -20,8 +20,15 @@ function Cart() {
                             }
                         </div>
                         <div className='checkout'>
-                            <h5>Subtotal ({cartCount} {cartCount > 1 ? 'items' : 'item'}) : ${TotalCartPrice} </h5>
-                            <button className='btn btn-warning'>Proceed to buy</button>
+                            <h5 className='fw-bolder '>SubTotal ({cartCount} {cartCount > 1 ? 'items' : 'item'})</h5>
+                            <div className='ps-5 text-start'>
+                                <p>Total Cost : ${TotalCartPrice}</p>
+                                <p>GST : 18%</p>
+                                <p>Shipping Charge : $50</p>
+                                <p className='fw-bolder'>You Pay : ${Math.round((TotalCartPrice * 100 * 1.18) + (50 * 100)) / 100}</p>
+                            </div>
+                            <div> <button className='btn btn-warning'>Proceed to buy</button>
+                            </div>
                         </div>
                     </div>
                 </div> : <EmptyCart />
